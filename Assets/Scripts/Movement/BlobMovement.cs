32b84668893c278dfab4bddd191f1851b851ac5d
Cifoviko <cifoviko@gmail.com>
2023-07-12 01:22:49 +0300

    added dummy behaviour for blobs

--- Assets/Scripts/Movement/BlobMovement.cs
+++ Assets/Scripts/Movement/BlobMovement.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class Blobmovement : MonoBehaviour
 {
+    [SerializeField] float Speed;
+    [SerializeField] GameObject target;
+
+    Rigidbody2D rigidbody;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidbody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 vector = target.transform.position - transform.position;
+        //Debug.Log("Move: " + vector);
+        rigidbody.AddForce(vector * Speed);
     }
 }
